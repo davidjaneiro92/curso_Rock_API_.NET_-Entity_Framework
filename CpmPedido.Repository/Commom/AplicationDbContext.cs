10e1edadb70a7f2c1a2d1ba4ffb5d5123c2225a0
davidjaneiro92 <davidjaneiro92@gmail.com>
2023-07-17 18:34:53 -0300

    conexao

--- CpmPedido.Repository/Commom/AplicationDbContext.cs
+++ CpmPedido.Repository/Commom/AplicationDbContext.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Azure.Core.HttpHeader;
 
 namespace CpmPedido.Repository
 {
     public class AplicationDbContext : DbContext
     {
+        //public DbSet<Cidade> Cidades { get; set; }
+        //public DbSet<Cliente> Clientes { get; set; }
+        //public DbSet<CategoriaProduto> CategoriasProdutos { get; set; }
+        //public DbSet<Produto> Produtos { get; set; }
+        //public DbSet<PromocaoProduto> PromocoesProdutos { get; set; }
+        //public DbSet<Combo> Combos { get; set; }
+        //public DbSet<Pedido> Pedidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        }
+
+        public ApplicationDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
 
     }
 }
--- CpmPedidos.API/Program.cs
+++ CpmPedidos.API/Program.cs
@@ -1,12 +1,25 @@
+using CpmPedido.Repository;
 using CpmPedidos.API;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ var DbConnection = new SqlConnection(builder.Configuration.GetConnectionString("conexao"));
+ var aplicationDbContext = new AplicationDbContext();
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+IServiceCollection serviceCollection = builder.Services.AddDbContext<AplicationDbContext>(option =>
+{
+    option.UseSqlServer(DbConnection, assembly => assembly.MigrationsAssembly(typeof(AplicationDbContext).Assembly.FullName));
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
